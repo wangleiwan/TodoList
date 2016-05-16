3369824dba6f2508245018266bfc6127815f344c
Lei Wang <wangleiwan@hotmail.com>
2016-05-16 13:00:26 -0400

    base setup

---
+++ src/app/containers/AppContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
